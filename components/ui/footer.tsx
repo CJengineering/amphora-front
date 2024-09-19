50d9636e87402a5fbfde5fc64afebdcfd853b3c0
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-09-19 16:07:23 +0100

    undeline on hover

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
               </Link>
             </div>
             <div className="grow text-sm text-zinc-500">&copy; jameelindex.mit.edu |All rights reserved.</div>
-            <div className="grow text-sm text-zinc-500"><Link href={'https://accessibility.mit.edu'}>Accessibility</Link></div>
+            <div className="grow text-sm text-zinc-500"><Link href={'https://accessibility.mit.edu'} className='hover:cursor-pointer hover:underline'>Accessibility</Link></div>
 
             {/* Social links */}
             {/* <ul className="flex space-x-4 mt-4 mb-1">
